115a289c99fc6180f1b4d3125ef0703a6ef17a84
theiago <theiago@live.com>
2022-09-14 23:05:32 -0300

    home atualizado e incluso botoes

--- app/src/main/java/br/com/mobile/sauceburguer/HomeActivity.kt
+++ app/src/main/java/br/com/mobile/sauceburguer/HomeActivity.kt
@@ -2,6 +2,9 @@ package br.com.mobile.sauceburguer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import br.com.mobile.sauceburguer.databinding.HomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -10,8 +13,27 @@ class HomeActivity : AppCompatActivity() {
         HomeBinding.inflate(layoutInflater)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+        when (id) {
+        R.id.action_buscar -> Toast.makeText(this, "Buscar", Toast.LENGTH_LONG).show()
+            R.id.action_atualizar -> Toast.makeText(this, "Atualizar", Toast.LENGTH_LONG).show()
+        }
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val params = this.intent.extras
+        val nome_usuario = params?.getString("nome_usuario")
+
     }
 }
